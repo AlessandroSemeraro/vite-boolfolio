524151437cd51ed4184c1277c08972366f47fce1
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-22 17:46:50 +0100

    added axios and api for projects back office

--- vue-project/src/components/AppMain.vue
+++ vue-project/src/components/AppMain.vue
@@ -4,9 +4,30 @@
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
-    
-}
+    name: 'AppMain',
+    data() {
+
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+    },
+        created() {
+            this.getProjects();
+        }
+    }
 </script>
 <style lang="">
     
